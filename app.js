ea0e0c2e864318f68fdc281f85fad3a5a62a0365
Shoki-Kusayanagi <67038346+Shoki-Kusayanagi@users.noreply.github.com>
2020-07-09 13:43:54 +0900

    7/9 中間コミット

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
+//BTBIメインモジュール（ここには定義とユーザ認証とルーティングのみ設定
+//その他のスクリプトは切り離す
+
+//expressの定義
 var express = require('express');
 var app = express();
-
 app.use(express.static('views'));
 app.use(express.urlencoded({extended: false}));
 
+//model定義
+var md_index = require('./views/model/mdl_index.js');
+
+
+
+//passportの定義（認証関係
 var passport = require('passport')
 app.use(passport.initialize()) //Expressを使用している場合はInitializeが必要
 
@@ -28,14 +37,14 @@ var flash = require('connect-flash');
 app.use(flash());
 
 var LocalStrategy = require('passport-local').Strategy;
+
+//認証処理
 passport.use(new LocalStrategy(function(username, password, done){
     // ここで username と password を確認して結果を返す
     if(username == 'root' && password == 'pass'){
-          console.log(username);
           return done(null, username);
       //↓にはPasswordチェック処理を実装してください。
     } else {
-          console.log(password)
           return done(null, false, { message: 'ユーザーIDまたはパスワードが間違っています。' })
       }
     }));
@@ -45,21 +54,18 @@ function checkAuthentication(req, res, next){
         return next();
     }
     else {  // 認証されていない
-      console.log('falld')
-        res.redirect('/login');  // ログイン画面に遷移
+      res.redirect('/login');  // ログイン画面に遷移
     }
 }
 
 //ここからルータとしての処理
+//ログイン・ログアウト画面以外は認証チェックを挟むこと
+//メイン
 app.get('/', checkAuthentication,(req, res) => {
-  console.log('OK!')
-  res.render('temp.ejs');
+  md_index.getDashboard((dash) => {res.render('index.ejs',{dash:dash})}) ;
 });
 
-app.get('/temp', (req, res) => {
-  getDashboard((dash) => {res.render('temp.ejs',{dash:dash})}) ;
-});
-
+//ログイン画面
 app.get('/login', (req, res) => {
   const errorMessage = req.flash('error');
   res.render('login.ejs', {
@@ -67,7 +73,7 @@ app.get('/login', (req, res) => {
   });
 });
 
-
+//ログイン処理
 app.post('/logon', passport.authenticate('local',
 {failureRedirect: '/login',failureFlash: true,
     badRequestMessage: '「ユーザID」と「パスワード」は必須入力です。'}),function(req, res){
@@ -76,97 +82,11 @@ app.post('/logon', passport.authenticate('local',
         res.redirect('/');
     });
 
+//ログアウト処理
 app.get('/logout', (req, res) => {
   req.logout();
   req.session.destroy();
   res.redirect('/login');
 });
-
-
-//ここから関数類
-const oracledb = require('oracledb')
-const config = {
-  user: 'BTBI',
-  password: 'BTBI',
-  connectString: '192.168.1.212:1521/BTDB.domain'
-}
-
-
-async function getDashboard(callback) {
-  let conn
-
-  try {
-    conn = await oracledb.getConnection(config)
-
-    var strSql = ""
-
-    var grh1Data =[];
-    //乗降グラフデータ取得
-
-    const result = await conn.execute(
-      'select * from V_ROUTE_CNT_EXPANSION'
-    )
-    var grh1Data =[];
-
-    for(let i=0; i<=result.rows.length-1;i++){
-      //オブジェクトに変数投入
-      row = await result.rows[i];
-      var grh =await {id: `widgetChart${i}`,
-      labels: [row[1],row[2],row[3],row[4],row[5],row[6],row[7]],
-      data: [row[8],row[9],row[10],row[11],row[12],row[13],row[14],],
-      route_name: '',
-      capcnt:row[8]};
-      //レコードから生成したオブジェクトをグラフデータ配列に追加
-      grh1Data.push(grh);
-    }
-
-    var label=[]
-    for (i=1; i<=31; i++){
-      label.push(i);
-    }
-    //売上実績データ取得
-    const data2 = await conn.execute(
-      'select * from V_ROUTE_AMOUNT_EXPANSION'
-    );
-    rw1= await data2.rows[0];
-    rw2= await data2.rows[1];
-
-    var grh2Data = await {id:'trafficChart',
-                    label:label,
-                    month:rw1[0],
-                    datacash:[rw1[5],rw1[6],rw1[7],rw1[8],rw1[9],rw1[10],rw1[11],rw1[12],rw1[13],rw1[14],rw1[15],rw1[16],rw1[17],rw1[18],rw1[19],rw1[20],rw1[21],rw1[22],rw1[23],rw1[24],rw1[25],rw1[26],rw1[27],rw1[28],rw1[29],rw1[30],rw1[31],rw1[32],rw1[33],rw1[34],rw1[35]],
-                    dataIC:[rw2[5],rw2[6],rw2[7],rw2[8],rw2[9],rw2[10],rw2[11],rw2[12],rw2[13],rw2[14],rw2[15],rw2[16],rw2[17],rw2[18],rw2[19],rw2[20],rw2[21],rw2[22],rw2[23],rw2[24],rw2[25],rw2[26],rw2[27],rw2[28],rw2[39],rw2[30],rw2[31],rw2[32],rw2[33],rw2[34],rw2[35]],
-                    dataCashamount:rw1[4],
-                    dataIcamount:rw2[4],
-                    data_type_name_cash:rw1[3],
-                    data_type_name_ic:rw2[3],
-                  };
-
-    //ヘッダデータ取得と戻り値用オブジェクト成型処理
-    const result3 = await conn.execute(
-      'select * from V_DASHBOARD'
-    );
-    console.log(result3.rows[0])
-    console.log(result3.rows[0][0])
-    var dash = await {date:result3.rows[0][0],
-                  TotalProfit1:result3.rows[0][1],
-                  TotalProfit2:result3.rows[0][2],
-                  TotalCustomer1:result3.rows[0][3],
-                  TotalCustomer2:result3.rows[0][4],
-                  grh1:grh1Data,
-                  grh2:grh2Data
-                };
-
-
-    callback(dash);
-    console.log(dash)
-  } catch (err) {
-    console.log('Ouch!', err)
-  } finally {
-    if (conn) { // conn assignment worked, need to close
-      await conn.close()
-    }
-  }
-}
 
 app.listen(4000);
--- views/assets/js/widgets.js
+++ views/assets/js/widgets.js
@@ -4,6 +4,7 @@
 
     // Counter Number
     $('.count').each(function () {
+        console.log('test')
         $(this).prop('Counter',0).animate({
             Counter: $(this).text()
         }, {
@@ -15,237 +16,62 @@
         });
     });
 
-    //WidgetChart 1
-    var ctx = document.getElementById( "widgetChart1" );
-    ctx.height = 150;
-    var myChart = new Chart( ctx, {
-        type: 'line',
-        data: {
-            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-            type: 'line',
-            datasets: [ {
-                data: [65, 59, 84, 84, 51, 55, 40],
-                label: 'Dataset',
-                backgroundColor: 'transparent',
-                borderColor: 'rgba(255,255,255,.55)',
-            }, ]
-        },
-        options: {
+    grh1.forEach(item => {
+      var ctx = document.getElementById( item.id );
+      ctx.height = 150;
+      var myChart = new Chart( ctx, {
+          type: 'line',
+          data: {
+              labels: item.labels,
+              type: 'line',
+              datasets: [ {
+                  data: item.data,
+                  label: '乗車人数',
+                  backgroundColor: 'transparent',
+                  borderColor: 'rgba(255,255,255,.55)',
+              }, ]
+          },
+          options: {
 
-            maintainAspectRatio: false,
-            legend: {
-                display: false
-            },
-            responsive: true,
-            scales: {
-                xAxes: [ {
-                    gridLines: {
-                        color: 'transparent',
-                        zeroLineColor: 'transparent'
-                    },
-                    ticks: {
-                        fontSize: 2,
-                        fontColor: 'transparent'
-                    }
-                } ],
-                yAxes: [ {
-                    display:false,
-                    ticks: {
-                        display: false,
-                    }
-                } ]
-            },
-            title: {
-                display: false,
-            },
-            elements: {
-                line: {
-                    borderWidth: 1
-                },
-                point: {
-                    radius: 4,
-                    hitRadius: 10,
-                    hoverRadius: 4
-                }
-            }
-        }
-    } );
-
-
-    //WidgetChart 2
-    var ctx = document.getElementById( "widgetChart2" );
-    ctx.height = 150;
-    var myChart = new Chart( ctx, {
-        type: 'line',
-        data: {
-            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-            type: 'line',
-            datasets: [ {
-                data: [1, 18, 9, 17, 34, 22, 11],
-                label: 'Dataset',
-                backgroundColor: '#63c2de',
-                borderColor: 'rgba(255,255,255,.55)',
-            }, ]
-        },
-        options: {
-
-            maintainAspectRatio: false,
-            legend: {
-                display: false
-            },
-            responsive: true,
-            scales: {
-                xAxes: [ {
-                    gridLines: {
-                        color: 'transparent',
-                        zeroLineColor: 'transparent'
-                    },
-                    ticks: {
-                        fontSize: 2,
-                        fontColor: 'transparent'
-                    }
-                } ],
-                yAxes: [ {
-                    display:false,
-                    ticks: {
-                        display: false,
-                    }
-                } ]
-            },
-            title: {
-                display: false,
-            },
-            elements: {
-                line: {
-                    tension: 0.00001,
-                    borderWidth: 1
-                },
-                point: {
-                    radius: 4,
-                    hitRadius: 10,
-                    hoverRadius: 4
-                }
-            }
-        }
-    } );
-
-
-
-    //WidgetChart 3
-    var ctx = document.getElementById( "widgetChart3" );
-    ctx.height = 150;
-    var myChart = new Chart( ctx, {
-        type: 'line',
-        data: {
-            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-            type: 'line',
-            datasets: [ {
-                data: [1, 18, 9, 17, 34, 22, 11],
-                label: 'Dataset',
-                backgroundColor: '#63c2de',
-                borderColor: 'rgba(255,255,255,.55)',
-            }, ]
-        },
-        options: {
-
-            maintainAspectRatio: false,
-            legend: {
-                display: false
-            },
-            responsive: true,
-            scales: {
-                xAxes: [ {
-                    gridLines: {
-                        color: 'transparent',
-                        zeroLineColor: 'transparent'
-                    },
-                    ticks: {
-                        fontSize: 2,
-                        fontColor: 'transparent'
-                    }
-                } ],
-                yAxes: [ {
-                    display:false,
-                    ticks: {
-                        display: false,
-                    }
-                } ]
-            },
-            title: {
-                display: false,
-            },
-            elements: {
-                line: {
-                    tension: 0.00001,
-                    borderWidth: 1
-                },
-                point: {
-                    radius: 4,
-                    hitRadius: 10,
-                    hoverRadius: 4
-                }
-            }
-        }
-    } );
-
-
-    //WidgetChart 4
-    var ctx = document.getElementById( "widgetChart4" );
-    ctx.height = 150;
-    var myChart = new Chart( ctx, {
-        type: 'line',
-        data: {
-            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-            type: 'line',
-            datasets: [ {
-                data: [1, 18, 9, 17, 34, 22, 11],
-                label: 'Dataset',
-                backgroundColor: '#63c2de',
-                borderColor: 'rgba(255,255,255,.55)',
-            }, ]
-        },
-        options: {
-
-            maintainAspectRatio: false,
-            legend: {
-                display: false
-            },
-            responsive: true,
-            scales: {
-                xAxes: [ {
-                    gridLines: {
-                        color: 'transparent',
-                        zeroLineColor: 'transparent'
-                    },
-                    ticks: {
-                        fontSize: 2,
-                        fontColor: 'transparent'
-                    }
-                } ],
-                yAxes: [ {
-                    display:false,
-                    ticks: {
-                        display: false,
-                    }
-                } ]
-            },
-            title: {
-                display: false,
-            },
-            elements: {
-                line: {
-                    tension: 0.00001,
-                    borderWidth: 1
-                },
-                point: {
-                    radius: 4,
-                    hitRadius: 10,
-                    hoverRadius: 4
-                }
-            }
-        }
-    } );
-
+              maintainAspectRatio: false,
+              legend: {
+                  display: false
+              },
+              responsive: true,
+              scales: {
+                  xAxes: [ {
+                      gridLines: {
+                          color: 'transparent',
+                          zeroLineColor: 'transparent'
+                      },
+                      ticks: {
+                          fontSize: 2,
+                          fontColor: 'transparent'
+                      }
+                  } ],
+                  yAxes: [ {
+                      display:false,
+                      ticks: {
+                          display: false,
+                      }
+                  } ]
+              },
+              title: {
+                  display: false,
+              },
+              elements: {
+                  line: {
+                      borderWidth: 1
+                  },
+                  point: {
+                      radius: 4,
+                      hitRadius: 10,
+                      hoverRadius: 4
+                  }
+              }
+          }
+      });
+    });
 
 
 } )( jQuery );
